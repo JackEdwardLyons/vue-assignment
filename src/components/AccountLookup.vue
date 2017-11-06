38591d4425f916d3b202e37a5725c8a34d797880
Jack Lyons <jack.edward.lyons@gmail.com>
2017-11-06 09:37:27 +0000

    Create event emitter to send selectedAccount to Properties section; allow UI to be updated on each click

--- src/components/AccountLookup.vue
+++ src/components/AccountLookup.vue
@@ -57,21 +57,4 @@ export default {
   display: flex;
 }
 
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
